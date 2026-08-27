348208e155a0858f5c053f8447f45e8c659befaa
haeckse <jane@doe>
2026-08-27 20:19:39 +0200

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.radiomii"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.1.0"
         buildConfigField("String", "GIT_HASH", "\"$gitHash\"")
     }
 
